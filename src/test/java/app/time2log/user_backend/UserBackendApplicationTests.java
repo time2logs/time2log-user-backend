be51ad06f5d63fa883f2445d86809a81f5e3a9df
ellodev <ello.developers@gmail.com>
2026-02-05 14:19:41 +0100

    initial commit

---
+++ src/test/java/app/time2log/user_backend/UserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.time2log.user_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
